6d7c088e74abf60d6779805df64e91fe25f69ea1
dhiego garcia <bianca.gp.bessa@hotmail.com>
2021-04-14 17:30:31 -0300

    Atividade continua 03

---
+++ ProjetoAC/app/src/main/java/br/com/biancabessa/ac02/AddProd.kt
@@ -0,0 +1,8 @@
+package br.com.biancabessa.ac02
+
+import java.io.Serializable
+
+class AddProd: Serializable {
+    var id: Long = 0
+    var nome = ""
+}
--- ProjetoAC/app/src/main/java/br/com/biancabessa/ac02/TelaInicialActivity.kt
+++ ProjetoAC/app/src/main/java/br/com/biancabessa/ac02/TelaInicialActivity.kt
@@ -1,20 +1,19 @@
 package br.com.biancabessa.ac02
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import androidx.appcompat.widget.SearchView
-import androidx.appcompat.widget.Toolbar
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
 import kotlinx.android.synthetic.main.toolbar.*
 
-class TelaInicialActivity : DebugActivity() {
+class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSelectedListener {
     fun toast(message: String?) =
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,46 +23,24 @@ class TelaInicialActivity : DebugActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.title = "Inicio"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        //supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        btn_tela01.setOnClickListener{
+        configurarMenuLateral()
 
-            val intent = Intent(this, TelaBotoes::class.java)
-            Toast.makeText(this, "Tela 01", Toast.LENGTH_SHORT).show()
+        recycle_prod?.layoutManager = LinearLayoutManager(this)
+    }
 
-            val params = Bundle()
-            params.putInt("flag", 1)
-            intent.putExtras(params)
-
-            startActivity(intent)
-
+    private var produtos = listOf<AddProd>()
+    override fun onResume() {
+        super.onResume()
+        produtos = AddProdService.getAddProd()
+        recycle_prod?.adapter = AddProdAdapter(produtos){
+            onClickProduto(it)
         }
+    }
 
-        btn_tela02.setOnClickListener{
-
-            val intent = Intent(this, TelaBotoes::class.java)
-            Toast.makeText(this, "Tela 02", Toast.LENGTH_SHORT).show()
-
-            val params = Bundle()
-            params.putInt("flag", 2)
-            intent.putExtras(params)
-
-            startActivity(intent)
-
-        }
-
-        btn_tela03.setOnClickListener{
-
-            val intent = Intent(this, TelaBotoes::class.java)
-            Toast.makeText(this, "Tela 03", Toast.LENGTH_SHORT).show()
-
-            val params = Bundle()
-            params.putInt("flag", 3)
-            intent.putExtras(params)
-
-            startActivity(intent)
-
-        }
+    fun onClickProduto(produto: AddProd){
+        Toast.makeText(this, "Clicou no produto ${produto.nome}", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -102,6 +79,58 @@ class TelaInicialActivity : DebugActivity() {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun configurarMenuLateral(){
+        var toogle = ActionBarDrawerToggle(
+            this,
+            layout_menu_lateral,
+            toolbar,
+            R.string.nav_open,
+            R.string.nav_close
+        )
+        layout_menu_lateral.addDrawerListener(toogle)
+        toogle.syncState()
+
+        nav_menu_lateral.setNavigationItemSelectedListener(this)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_opcao01 -> {
+                val intent = Intent(this, TelaBotoes::class.java)
+                Toast.makeText(this, "Tela 01", Toast.LENGTH_SHORT).show()
+
+                val params = Bundle()
+                params.putInt("flag", 1)
+                intent.putExtras(params)
+
+                startActivity(intent)
+            }
+
+            R.id.nav_opcao02 -> {
+                val intent = Intent(this, TelaBotoes::class.java)
+                Toast.makeText(this, "Tela 02", Toast.LENGTH_SHORT).show()
+
+                val params = Bundle()
+                params.putInt("flag", 2)
+                intent.putExtras(params)
+
+                startActivity(intent)
+            }
+
+            R.id.nav_opcao03 -> {
+                val intent = Intent(this, TelaBotoes::class.java)
+                Toast.makeText(this, "Tela 03", Toast.LENGTH_SHORT).show()
+
+                val params = Bundle()
+                params.putInt("flag", 3)
+                intent.putExtras(params)
+
+                startActivity(intent)
+            }
+        }
+        layout_menu_lateral.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
